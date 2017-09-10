65e8e1d35a0e39112f6806aa26d751ed635fccb7
sunhui04 <sunhui04@meituan.com>
2017-09-10 17:42:25 +0800

    pagination add bem

--- src/components/amir-pagination/index.vue
+++ src/components/amir-pagination/index.vue
@@ -1,33 +1,33 @@
 <template>
-	<div class="amir-pagination-container">
+	<div class="amir--pagination">
 		<a :class="prevClass" @click="switchPrevPage"> < 上一页</a>
 
-		<a v-if="currentPage > 1" @click="switchPage">1</a>
+		<a v-if="currentPage > 1" @click="switchPage" :class="aClassObject">1</a>
 
 		<template v-if="currentPage > 2+1+1">
-			 <span class="dot">...</span>
+			 <span :class="dotClassObject">...</span>
 		</template>
 
 		<template v-for="item in beforeItems">
-			<a v-if="currentPage > item + 1" @click="switchPage" class="neighbor-page">{{currentPage - item}}</a>
+			<a v-if="currentPage > item + 1" @click="switchPage" :class="aClassObject">{{currentPage - item}}</a>
 		</template>
 
-		<a class="active">{{currentPage}}</a>
+		<a :class="activeClassObject">{{currentPage}}</a>
 
 		<template v-for="item in afterItems">
-			<a v-if="currentPage + item < totalPage" @click="switchPage" class="neighbor-page">{{currentPage + item}}</a>
+			<a v-if="currentPage + item < totalPage" @click="switchPage" :class="aClassObject">{{currentPage + item}}</a>
 		</template>
 
 		<template v-if="currentPage+2+1 < totalPage">
-			<span class="dot">...</span>
+			<span :class="dotClassObject">...</span>
 		</template>
 
-		<a v-if="currentPage < totalPage" @click="switchPage">{{totalPage}}</a>
+		<a v-if="currentPage < totalPage" @click="switchPage" :class="aClassObject">{{totalPage}}</a>
 
 		<a :class="nextClass" @click="switchNextPage">下一页 > </a>
 
-		<div class="goto-container" v-if="goto">
-			<span class="goto">前往 </span><input type="text" v-model="destinationPage" class="goto-input">页 <amir-button @click="goSwitchPage"></amir-button>
+		<div :class="gotoClassObject" v-if="goto">
+			<span :class="spanClassObject">前往 </span><input type="text" v-model="destinationPage" :class="inputClassObject">页 <amir-button @click="goSwitchPage"></amir-button>
 		</div>
 		
 	</div>
@@ -43,7 +43,26 @@
 			return {
 				beforeItems: [2,1],		//倒叙，保证输出的页码能以正序呈现
 				afterItems: [1,2],
-				destinationPage: ''
+				destinationPage: '',
+				aClassObject: {
+					'amir--pagination__a': true
+				},
+				dotClassObject: {
+					'amir--pagination__dot': true
+				},
+				activeClassObject: {
+					'amir--pagination__a': true,
+					'amir--pagination__a--active': true
+				},
+				gotoClassObject: {
+					'amir--pagination__goto': true
+				},
+				spanClassObject: {
+					'amir--pagination__goto__span': true
+				},
+				inputClassObject: {
+					'amir--pagination__goto__input': true
+				}
 			}
 		},
 		props: {
@@ -63,14 +82,16 @@
 		computed: {
 			prevClass: function() {
 				return {
-					prev: true,
-					disabled: this.currentPage === 1
+					'amir--pagination__a': true,
+					'amir--pagination__a--prev': true,
+					'amir--pagination__a--disabled': this.currentPage === 1
 				}
 			},
 			nextClass: function() {
 				return {
-					next: true,
-					disabled: this.currentPage === this.totalPage
+					'amir--pagination__a': true,
+					'amir--pagination__a--next': true,
+					'amir--pagination__a--disabled': this.currentPage === this.totalPage
 				}
 			}
 		},
@@ -103,11 +124,14 @@
 	}
 </script>
 
-<style lang="less" scoped>
-	@mainColor: #3dc6b6;
-	@disabledColor: #e5e5e5;
-	.amir-pagination-container {
-		a {
+<style lang="less">
+	@import '../common.less';
+	@b pagination {
+		@e a:hover {
+			border: 1px solid @mainColor;
+			cursor:pointer;
+		}
+		@e a {
 			width:35px;
 			height: 35px;
 			line-height: 35px;
@@ -119,47 +143,51 @@
 			border:1px solid @disabledColor;
 			border-radius: 2px;
 
-			&.active {
+			@m prev {
+				width:70px;
+				color:#333;
+			}
+			@m next {
+				width:70px;
+				color:#333;
+			}
+			@m prev:hover {
+				border: 1px solid #e5e5e5;
+			}
+			@m next:hover {
+				border: 1px solid #e5e5e5;
+			}
+			@m disabled {
+				background:@disabledColor;
+				border:1px solid @disabledColor;
+				cursor:not-allowed;
+			}
+			@m disabled:hover {
+				cursor:not-allowed;
+			}
+
+			@m active {
 				background:@mainColor;
 				color:#fff;
 				border:1px solid @mainColor;
-
 			}
-
-			&:hover {
-				border: 1px solid @mainColor;
-				cursor:pointer;
-			}
-
-			&.prev, &.next {
-				width:70px;
-				color:#333;
-
-				&.disabled {
-					background:@disabledColor;
-					border:1px solid @disabledColor;
-					cursor:not-allowed;
-				}
-				&:hover {
-					border: 1px solid #e5e5e5;
-				}
-			}
-
 		}
 
-		.dot {
+		@e dot {
 			color:#333;
 		}
 
-		.goto-container {
+		@e goto {
 			display: inline-block;
 			font-size: 12px;
 			color:#666;
-			.goto {
+
+			@e item {
 				font-size: 12px;
 				color:#666;
 			}
-			.goto-input {
+
+			@e input {
 				width:33px;
 				height:30px;
 				line-height: 30px;
