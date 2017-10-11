cc72b15f09998e2090a06bc1591de428cde95603
sunhui04 <sunhui04@meituan.com>
2017-10-11 11:22:20 +0800

    add merge

---
+++ build/webpack.common.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'vue': 'vue/dist/vue.js'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					less: 'vue-style-loader!css-loader!less-loader',
+					postcss: [
+						require('postcss-cssnext')({browsers: ['last 2 versions']}),
+						require('postcss-bem-fix')({
+							defaultNamespace: 'amir',
+							style: "bem",
+							separators: {
+								'descendent': '__',
+								'modifier': '--'
+							},
+							shortcuts: {
+								'component': 'b',
+								'descendent': 'e',
+								'modifier': 'm'
+							}
+						}),
+						// require('postcss-nested-ancestors'),
+						require('postcss-nested')
+					],
+					extractCSS: true
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: [/node_modules/]
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader"
+				})
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+			inject: true
+		}),
+		new ExtractTextPlugin('amir.css')
+	]
+}
--- build/webpack.prod.split.config.js
+++ build/webpack.prod.split.config.js
@@ -1,18 +1,18 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common.config');
 
-var componentNames = ['button', 'confirm', 'dialog', 'input', 'loading', 'pagination', 'select', 'table', 'textarea', 'toast', 'upload'];
+const componentNames = ['button', 'confirm', 'dialog', 'input', 'loading', 'pagination', 'select', 'table', 'textarea', 'toast', 'upload'];
 
 function generateEntry(componentNames){
-	var object = {};
+	let object = {};
 	componentNames.forEach(function(item, index) {
 		object[item] = path.resolve(__dirname, '../src/components/amir-' + item + '/index.vue' );
 	});
 	return object;
 }
 
-module.exports = {
+module.exports = merge(commonConfig, {
 	entry: generateEntry(componentNames),
 	output: {
 		filename: '[name]/index.js',
@@ -21,12 +21,6 @@ module.exports = {
 		libraryTarget: 'umd',
 		publicPath: '/'
 	},
-	resolve: {
-		extensions: ['.js', '.vue', '.json'],
-		alias: {
-			'vue': 'vue/dist/vue.js'
-		}
-	},
 	externals: {
 		vue: {
 			root: 'Vue',
@@ -34,55 +28,5 @@ module.exports = {
 			commonjs: 'vue',
 			commonjs2: 'vue'
 		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					less: 'vue-style-loader!css-loader!less-loader',
-					postcss: [
-						require('postcss-cssnext')({browsers: ['last 2 versions']}),
-						require('postcss-bem-fix')({
-							defaultNamespace: 'amir',
-							style: "bem",
-							separators: {
-								'descendent': '__',
-								'modifier': '--'
-							},
-							shortcuts: {
-								'component': 'b',
-								'descendent': 'e',
-								'modifier': 'm'
-							}
-						}),
-						// require('postcss-nested-ancestors')(),
-						require('postcss-nested')
-					],
-					extractCSS: true
-				}
-			},
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: [/node_modules/]
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: "css-loader"
-				})
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: './src/index.html',
-			inject: true
-		}),
-		new ExtractTextPlugin('amir.css')
-	]
-}
+	}
+})
